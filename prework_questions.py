0c6628370d2d6c5a42c84e0fa4c5d302cd0c90e7
AustinKobie <110858945+AustinKobie@users.noreply.github.com>
2023-01-11 23:36:52 -0500

    Create prework_questions.py

---
+++ prework_questions.py
@@ -0,0 +1,14 @@
+#Question 1:
+#Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+
+def hello_name(user_name):
+    print('hello' '',user_name)
+
+hello_name(user_name= 'USERNAME!')
+
+#Question 2:
+#Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing.
+def first_odds():
+    return nothing
+first_odds= for i in range(1,100):
+    
