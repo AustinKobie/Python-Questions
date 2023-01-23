cd187bb454ae00678e36e548a36503d84a175867
AustinKobie <110858945+AustinKobie@users.noreply.github.com>
2023-01-22 23:45:45 -0500

    Update prework_questions.py

--- prework_questions.py
+++ prework_questions.py
@@ -9,6 +9,31 @@ hello_name(user_name= 'USERNAME!')
 #Question 2:
 #Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing.
 def first_odds():
-    return nothing
-first_odds= for i in range(1,100):
+    first, last = 1, 100
+    for i in range(first, last + 1):
+        if i % 2 != 0:
+            print(i, last = " ")
+            return None
+        
+
+
+
+#Question 3:
+#Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
+def max_num_in_list(a_list):
+    a_list[1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print ("max number in list", a_list)
+    return max(a_list)
     
+    
+    
+    
+#Question 4:
+#Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
+def is_leap_year(a_year):
+
+
+
+#Question 5:
+#Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
+ def is_consecutive(a_list):
